bb7d4e55a7515da6b449013d62834d41c9080c5d
shi <shizhongtao@cmss.chinamobile.com>
2018-04-08 10:05:19 +0800

    首次提交
    Change-Id: Id62211086fd24d9a6b1bfd6c978b15d5cf26617d

---
+++ src/main/java/cn/com/binging/simpleflowdemo/SimpleFlowDemoApplication.java
@@ -0,0 +1,12 @@
+package cn.com.binging.simpleflowdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleFlowDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleFlowDemoApplication.class, args);
+	}
+}
